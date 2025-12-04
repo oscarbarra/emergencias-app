f502ff7e6c8d9a06c766f22130ce4bd36df0f9eb
obarra2023 <obarra2023@alu.uct.cl>
2025-12-03 23:42:25 -0300

    solución conflicto en los estilos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 
-// import './styles/App.css';
+import './styles/App.css';
 import PcView from './pages/Pc-View/index';
 import MobilView from './pages/Mobile-View/index';
 import { useIsMobile } from "./hooks/useIsMobile";
+import { useEffect } from "react";
 
 function App() {
   const isMobile = useIsMobile();
+
+  useEffect(() => {
+    document.body.classList.remove("pc", "mobile");
+    document.body.classList.add(isMobile ? "mobile" : "pc");
+  }, [isMobile]);
+  
   return isMobile ? <MobilView /> : <PcView />;
 }
 
